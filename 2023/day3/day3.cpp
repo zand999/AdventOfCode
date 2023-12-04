65af39566d4024c18c951a51e0ef68de25434eff
zand999 <999zand@gmail.com>
2023-12-04 15:54:31 -0500

    day3part1

--- 2023/day3/day3.cpp
+++ 2023/day3/day3.cpp
@@ -26,7 +26,7 @@ bool isSymbol(char a){
 
 bool isNum(char a){
 
-    if(a >= 48 || a <= 57 ){
+    if(a >= 48 && a <= 57 ){
         return true;
     }else{
         return false;
@@ -65,7 +65,7 @@ int main(){
                 grid[i][j] = (char)stringinput.at(j-1);
 
             }
-            cout << "\n";
+            //cout << "\n";
         }
 
 
@@ -77,37 +77,205 @@ int main(){
 	}else{
 		cout << "file did not open";
 	}
-
-    int top[3] = {0,0,0};
-    int bottom[3] = {0,0,0};
+    
+    
 
     for(int i = 1;i< 141;i++){
         
         for(int j=1;j<141;j++){
-            int numdata[3] = {0,0,0};
             
-            //top
+            
+            string num = "";
             int offset = 0;
-            while(isNum(grid[i+1][j+1-offset])){
-                offset--;
-                if(offset > -1){
-                    numdata[2-offset] = 1;
+            if(isSymbol(grid[i][j])){
+                cout << grid[i][j] << ": ";
+                //top
+                if(isNum(grid[i-1][j])){
+                    //cout << grid[i-1][j] << " "<<isNum(grid[i-1][j]) << endl;
+                    while(isNum(grid[i-1][j-offset])){
+                        if(j-offset+1 < 0){
+                            offset++;
+                            break;
+                        }
+                        offset++;
+                        
+                    }
+                    offset--;
+
+                    //cout << grid[i-1][j-offset] << endl;
+                    while(isNum(grid[i-1][j-offset])){
+                        //cout << grid[i-1][j-offset];
+                        num += grid[i-1][j-offset];
+                        if(j-offset+1 > 141)break;
+                        offset--;
+                    }
+                    //cout << endl;
+                    if(num.size() != 0) {
+                        cout <<" tt-num:" << num;
+                        total += stoi(num);
+                    }
+
+
+                }else{
+                    //topleft
+                    offset = 0;
+                    while(isNum(grid[i-1][j-offset-1])){
+                        if(j-offset-2 < 0){
+                            offset++;
+                            break;
+                        }
+                        offset++;
+                    }
+                    offset--;
+                    
+                    while(isNum(grid[i-1][j-offset-1])){
+                        num += grid[i-1][j-offset-1];
+                        if(j-offset+1 > 141)break;
+                        offset--;
+                    }
+                    //cout << " off: "<< offset;
+                    
+                    if(num.size() != 0) {
+                        cout <<" tl-num:" << num;
+                        total += stoi(num);
+                    }
+
+                    //topright
+                    offset = 0;
+                    num = "";
+                    while(isNum(grid[i-1][j-offset+1])){
+                        num += grid[i-1][j-offset+1];
+                        if(j-offset+1 > 141)break;
+                        offset--;
+                    }
+                    //cout << " off: "<< offset;
+                    
+                    if(num.size() != 0) {
+                        cout <<" tr-num:" << num;
+                        total += stoi(num);
+                    }
+
                 }
                 
+                num = "";
+                offset = 0;
+
+                //left
+                offset = 0;
+                while(isNum(grid[i][j-offset-1])){
+                    if(j-offset-2 < 0){
+                        offset++;
+                        break;
+                    }
+                    offset++;
+                }
+                offset--;
+                
+                while(isNum(grid[i][j-offset-1])){
+                    num += grid[i][j-offset-1];
+                    if(j-offset+1 > 141)break;
+                    offset--;
+                }
+                //cout << " off: "<< offset;
+                
+                if(num.size() != 0) {
+                    cout <<" ll-num:" << num;
+                    total += stoi(num);
+                }
+
+                //right
+                offset = 0;
+                num = "";
+                while(isNum(grid[i][j-offset+1])){
+                    num += grid[i][j-offset+1];
+                    if(j-offset+1 > 141)break;
+                    offset--;
+                }
+                //cout << " off: "<< offset;
+                
+                if(num.size() != 0) {
+                    cout <<" rr-num:" << num;
+                    total += stoi(num);
+                }
+
+                //bottom
+                num = "";
+                if(isNum(grid[i+1][j])){
+                    //cout << grid[i-1][j] << " "<<isNum(grid[i-1][j]) << endl;
+                    while(isNum(grid[i+1][j-offset])){
+                        if(j-offset+1 < 0){
+                            offset++;
+                            break;
+                        }
+                        offset++;
+                        
+                    }
+                    offset--;
+
+                    //cout << grid[i-1][j-offset] << endl;
+                    while(isNum(grid[i+1][j-offset])){
+                        //cout << grid[i-1][j-offset];
+                        num += grid[i+1][j-offset];
+                        if(j-offset+1 > 141)break;
+                        offset--;
+                    }
+                    //cout << endl;
+                    if(num.size() != 0) {
+                        cout <<" bb-num:" << num;
+                        total += stoi(num);
+                    }
+
+
+                }else{
+                    //bottomleft
+                    num = "";
+                    offset = 0;
+                    while(isNum(grid[i+1][j-offset-1])){
+                        
+                        if(j-offset-2 < 0){
+                            offset++;
+                            break;
+                        }
+                        offset++;
+                    }
+                    offset--;
+                    
+                    while(isNum(grid[i+1][j-offset-1])){
+                        
+                        num += grid[i+1][j-offset-1];
+                        if(j-offset+1 > 141)break;
+                        offset--;
+                    }
+                    //cout << " off: "<< offset;
+                    
+                    if(num.size() != 0) {
+                        cout << " here";
+                        cout <<" bl-num:" << num;
+                        total += stoi(num);
+                    }
+
+                    //bottom right
+                    offset = 0;
+                    num = "";
+                    while(isNum(grid[i+1][j-offset+1])){
+                        num += grid[i+1][j-offset+1];
+                        if(j-offset+1 > 141)break;
+                        offset--;
+                    }
+                    //cout << " off: "<< offset;
+                    
+                    if(num.size() != 0) {
+                        cout <<" br-num:" << num;
+                        total += stoi(num);
+                    }
+
+                }
+                cout << endl;
             }
-
-
-            //left
-
-            //right
-
-
-            //bottom
-
         }
         
     }
-
+    
 
 
     cout << "Total: " <<  total << "\n";
