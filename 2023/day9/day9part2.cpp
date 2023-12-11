f25926862aa171e09953f282d57b45d49e7d4e4e
zand999 <999zand@gmail.com>
2023-12-11 15:15:22 -0500

    day9 part1+1

---
+++ 2023/day9/day9part2.cpp
@@ -0,0 +1,121 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <regex>
+using namespace std;
+
+void printArray(long *array){
+    cout << "(";
+    for(int i = 0; i < 21; i++){
+        cout << array[i] << ",";
+    }
+    cout << ")\n";
+}
+
+long *generateDiff(long *list, int ln){
+    long *intdiff = new long[ln-1];
+    for(int i = 0;i < ln-1;i++){
+        intdiff[i] = list[i+1] - list[i];
+    }
+    return intdiff;
+}
+
+long nextValue(long *list, int ln){
+
+    long find0;
+
+    if(ln == 1){
+        return list[0];
+    }
+    for(int i = 0;i < ln;i++){
+        find0 += list[i];
+    }
+    if(find0 == 0){
+        return  0;
+    }
+
+    long *intdiff = generateDiff(list, ln);
+
+
+    long retval = nextValue(intdiff, ln-1);
+
+    free(intdiff);
+    
+
+    return list[0]-retval;
+
+}
+
+/*long nextValue(long *list, int ln, long &lastnum, long &total){
+
+    if(ln == 1){
+        return list[0];
+    }
+    for(int i = 0;i < ln;i++){
+        find0 += list[i];
+    }
+    if(find0 == 0){
+        return  0;
+    }
+
+    
+
+    long *intdiff = new long[ln-1];
+
+    for(int i = 0;i < ln-1;i++){
+        intdiff[i] = list[i+1] - list[i];
+    }
+
+    long retval = nextValue(intdiff, ln-1, list[ln-1]);
+    long nextval = retval + list[ln-1];
+    total += nextval
+
+    free(intdiff);
+    
+
+    return nextval;
+
+}
+
+long getNextValue(long *list, int ln){
+    long val = 0, ;
+
+    return 
+}*/
+
+
+int main(){
+
+    ifstream input;
+    string stringinput;
+
+    long total = 0;;
+    long temp = 0;
+    long *sequence = new long[21];
+
+    input.open("input.txt");
+
+    if(input.is_open()){
+        while(!input.eof()){
+            for(int i = 0; i <  21; i++){
+                input >> temp;
+                //cout << temp;
+                sequence[i] = temp;
+            }
+            printArray(sequence);
+            temp = nextValue(sequence,21);
+            cout << "->" << temp << "     Total: " << total << endl;
+            total += temp;
+
+            
+
+        }
+
+    }else{
+        cout << "Failed to open file\n";
+    }
+
+    cout << endl << total << endl;
+}
